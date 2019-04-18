452d24b3245facfd3de16a891c5fc89b5f8de0dd
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-04-18 20:58:16 +0200

    Add sha256 of file

--- git-ignore.rb
+++ git-ignore.rb
@@ -2,7 +2,7 @@ class GitIgnore < Formula
   desc "Quickly and easily list and fetch .gitignore templates from www.gitignore.io"
   homepage "https://github.com/sondr3/git-ignore"
   url "https://github.com/sondr3/git-ignore/releases/download/v1.0.0-pre/git-ignore-v1.0.0-pre-x86_64-apple-darwin.tar.gz"
-  sha256 ""
+  sha256 "844f0f4cf27d0df538ce250cd30983a0122effae4a029dfd1fd7d2d391de0cc9"
   version "1.0.0-pre"
 
   def install
