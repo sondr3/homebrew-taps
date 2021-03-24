5ad94a0809ac472c20e1f4417917cc001d4c9222
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-03-24 14:23:26 +0100

    Update to latest release
    This fixes sondr3/git-ignore#6

--- git-ignore.rb
+++ git-ignore.rb
@@ -1,16 +1,16 @@
 class GitIgnore < Formula
   desc "Quickly and easily list and fetch .gitignore templates from www.gitignore.io"
   homepage "https://github.com/sondr3/git-ignore"
-  url "https://github.com/sondr3/git-ignore/releases/download/v1.1.1/git-ignore-x86_64-apple-darwin.tar.gz"
-  sha256 "66ca0f98dce68319aaaa6ace9b0d7b1b5a17f95c9a60cbabeb57c83b55686d10"
-  version "1.1.1"
+  url "https://github.com/sondr3/git-ignore/releases/download/v1.1.2/git-ignore-x86_64-apple-darwin.tar.gz"
+  sha256 "164e2a733826d069e54aa90040de7195334d935f45f9676d763b2a81689e7bbb"
+  version "1.1.2"
 
   def install
     bin.install "git-ignore"
-    # man1.install "git-ignore.1"
+    man1.install "git-ignore.1"
   end
 
   test do
-    assert_equal %x('#{bin}/git-ignore -V'), "git ignore 1.1.1\n"
+    assert_equal %x('#{bin}/git-ignore -V'), "git ignore 1.1.2\n"
   end
 end
