18e782642d6c1452a8a67093c9f9335489a98be2
Sondre Aasemoen <sondre@eons.io>
2022-10-21 08:33:23 +0200

    Update git-ignore to v1.2.2

--- git-ignore.rb
+++ git-ignore.rb
@@ -1,9 +1,9 @@
 class GitIgnore < Formula
   desc "Quickly and easily list and fetch .gitignore templates from www.gitignore.io"
   homepage "https://github.com/sondr3/git-ignore"
-  url "https://github.com/sondr3/git-ignore/releases/download/v1.2.1/git-ignore-x86_64-apple-darwin.tar.gz"
-  sha256 "5f3527b907bcc595cc49ce4c002d195bb14162e64a6111455e84c337f9632f55"
-  version "1.2.1"
+  url "https://github.com/sondr3/git-ignore/releases/download/v1.2.2/git-ignore-x86_64-apple-darwin.tar.gz"
+  sha256 "3fccba7f8067bc45224de33465e195180d34924e929c72a24d45088b433e3263"
+  version "1.2.2"
 
   def install
     bin.install "git-ignore"
@@ -14,6 +14,6 @@ class GitIgnore < Formula
   end
 
   test do
-    assert_equal "git ignore 1.2.1", shell_output("#{bin}/git-ignore -V").strip
+    assert_equal "git ignore 1.2.2", shell_output("#{bin}/git-ignore -V").strip
   end
 end
