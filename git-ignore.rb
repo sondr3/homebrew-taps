1cadd574aa7e7b637a708d558abc85262310e2cf
Sondre Aasemoen <sondre@eons.io>
2024-02-21 13:12:06 +0100

    Correct path to git-ignore versions

--- git-ignore.rb
+++ git-ignore.rb
@@ -4,12 +4,12 @@ class GitIgnore < Formula
   version "1.3.3"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/sondr3/git-ignore/releases/download/#{version}/git-ignore-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/sondr3/git-ignore/releases/download/v#{version}/git-ignore-x86_64-apple-darwin.tar.gz"
     sha256 "decdb652ec548172ed8f25e4d067ccc0f107220e6a6375303ad6ee7fe3d86ce9"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/sondr3/git-ignore/releases/download/#{version}/git-ignore-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/sondr3/git-ignore/releases/download/v#{version}/git-ignore-x86_64-apple-darwin.tar.gz"
     sha256 "d2caf3a1393c2fa2759f07c9152aac4fa2d5bbb6357ca733add7565d321b60d3"
   end
 
