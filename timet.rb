fefc7a4a537f8922e8f14c8429ea6701bd05a37c
Sondre Aasemoen <sondre@eons.io>
2024-02-29 22:30:01 +0100

    Bump timet to v0.2.0

--- timet.rb
+++ timet.rb
@@ -1,16 +1,16 @@
 class Timet < Formula
   desc "Automate your Timet fetching"
   homepage "https://github.com/sondr3/timet-rs"
-  version "0.1.2"
+  version "0.2.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/sondr3/timet-rs/releases/download/v#{version}/timet-x86_64-apple-darwin.tar.gz"
-    sha256 "46a77e2192c9c220b7c11398fc5bf79867cc9218107c9ed3eaac711792001f3c"
+    sha256 "998b1feae5224b99f5c515826873de5728e3e97a1b84e5ee3ffdfb3e425a6da1"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/sondr3/timet-rs/releases/download/v#{version}/timet-aarch64-apple-darwin.tar.gz"
-    sha256 "1170786ad2d832791b3e6c8754f71e8abdde23ec2c81b7c582adf5fe27469574"
+    sha256 "da1e9c06a02215b609fc1be580dd398c1ac77d8a87a0cd9f590e557c2286108e"
   end
 
   def install
