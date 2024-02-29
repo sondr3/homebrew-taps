9d31554a8f2fba68f21c40d407cdf3c70407de62
Sondre Aasemoen <sondre@eons.io>
2024-02-29 15:37:12 +0100

    Bump timet to v0.1.2

--- timet.rb
+++ timet.rb
@@ -1,9 +1,17 @@
 class Timet < Formula
   desc "Automate your Timet fetching"
   homepage "https://github.com/sondr3/timet-rs"
-  url "https://github.com/sondr3/timet-rs/releases/download/v0.1.0/timet-x86_64-apple-darwin.tar.gz"
-  sha256 "ad64f0d90edef62e61dcf3021062f6a537608122fc79f4f057479ddbf98ff3b1"
-  version "v0.1.0"
+  version "0.1.2"
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/sondr3/timet-rs/releases/download/v#{version}/timet-x86_64-apple-darwin.tar.gz"
+    sha256 "46a77e2192c9c220b7c11398fc5bf79867cc9218107c9ed3eaac711792001f3c"
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/sondr3/timet-rs/releases/download/v#{version}/timet-aarch64-apple-darwin.tar.gz"
+    sha256 "1170786ad2d832791b3e6c8754f71e8abdde23ec2c81b7c582adf5fe27469574"
+  end
 
   def install
     bin.install "timet"
@@ -14,6 +22,6 @@ class Timet < Formula
   end
 
   test do
-    assert_equal "timet 0.1.0", shell_output("#{bin}/timet -V").strip
+    assert_equal "timet #{version}", shell_output("#{bin}/timet -V").strip
   end
 end
