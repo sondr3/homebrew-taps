a24b0a1511aea51dbb941f22e7fddc88cce178e0
Sondre Aasemoen <sondre@eons.io>
2024-07-23 14:22:29 +0200

    Bump git-ignore to v1.4.0

--- git-ignore.rb
+++ git-ignore.rb
@@ -1,16 +1,16 @@
 class GitIgnore < Formula
   desc "Quickly and easily list and fetch .gitignore templates from www.gitignore.io"
   homepage "https://github.com/sondr3/git-ignore"
-  version "1.3.3"
+  version "1.4.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/sondr3/git-ignore/releases/download/v#{version}/git-ignore-x86_64-apple-darwin.tar.gz"
-    sha256 "decdb652ec548172ed8f25e4d067ccc0f107220e6a6375303ad6ee7fe3d86ce9"
+    sha256 "142f61b7c2957904e9ab39e88abbab576d46f82dbe6effb27cd970268e42768c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/sondr3/git-ignore/releases/download/v#{version}/git-ignore-aarch64-apple-darwin.tar.gz"
-    sha256 "d2caf3a1393c2fa2759f07c9152aac4fa2d5bbb6357ca733add7565d321b60d3"
+    sha256 "d9d8b9a71d7ef00415af2b5dca9fcd940afdc4c08d84b122903817569100e291"
   end
 
   def install
